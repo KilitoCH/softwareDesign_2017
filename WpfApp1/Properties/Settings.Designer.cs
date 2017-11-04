481faef49a6f91b3705e16b201138c132c9f99f4
Zixin Chen <zixinhustEIC@outlook.com>
2017-11-05 02:01:41 +0800

    进行了一些关于画图API的测试

--- WpfApp1/Properties/Settings.Designer.cs
+++ WpfApp1/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WpfApp1.Properties
+namespace SoftwareDesign_2017.Properties
 {
 
 
