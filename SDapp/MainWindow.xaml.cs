16643a272c9d6cba8305ec0352c870cba24ecb40
Zixin Chen <zixinhustEIC@outlook.com>
2017-11-15 23:41:43 +0800

    此次提交完善了参数输入功能和图像实时更新保存功能。

--- SDapp/MainWindow.xaml.cs
+++ SDapp/MainWindow.xaml.cs
@@ -5,13 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SoftwareDesign_2017
 {
@@ -20,14 +15,68 @@ namespace SoftwareDesign_2017
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Context context = new Context();
+
         public MainWindow()
         {
             InitializeComponent();
-            Paint paint = new Paint();
-            //tabItem2_canvas.AddVisual(paint.Draw(paint.TestSinSequence,true,false,Type.Line));
-            SavePic savePic = new SavePic();
-            savePic.SaveVisual(paint.Draw(paint.TestSinSequence, true, false, Type.Line));
-            //tabItem2_grid.Children.Add(paint.DrawCoordinate(new Point(10, 400), new Point(500, 400), new Point(10, 10)));//笛卡尔坐标系
         }
+
+#region tabItem1的事件
+        //选项卡1中的文本框的内容发生改变时触发的事件
+        private void tabItem1_TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var control = sender as TextBox;
+            Sin_Sequence_Generate sin_Sequence_Generate = new Sin_Sequence_Generate();
+            try
+            {
+                this.context.pointsForTabItem1 = sin_Sequence_Generate.Sin_Sequence(Convert.ToDouble(control.Text));
+            }
+            catch (Exception)
+            {
+                if (control.Text == "请输入相关频率") ;
+                else
+                    MessageBox.Show("输入浮点数");
+            }            
+        }
+
+        //选项卡1中的提交按钮（tabItem1_Button1）被点击时触发的事件
+        private void tabItem1_Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Paint paint = new Paint();
+            if (context.pointsForTabItem1 != null)
+            {
+                context.visualForTabItem1 = paint.DrawVisual(this.context.pointsForTabItem1, true, false, Type.Line);
+                tabItem1_canvas.RemoveAll();
+                tabItem1_canvas.AddVisual(context.visualForTabItem1);
+            }
+            else
+                MessageBox.Show("请先输入参数再画图", "提示");
+        }
+
+        //选项卡1中的文本框获取焦点时触发的事件
+        private void tabItem1_TextBox1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            tabItem1_TextBox1.SelectAll();
+        }
+
+        //当鼠标左键在选项卡1中的文本框中按下时触发的事件
+        private void tabItem1_TextBox1_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var control = sender as TextBox;
+            if (control == null)
+                return;
+            Keyboard.Focus(control);
+            e.Handled = true;
+        }
+
+        //选项卡1中的保存图片按钮被按下时触发的事件
+        private void tabItem1_Button2_Click(object sender, RoutedEventArgs e)
+        {
+            SavePic savePic = new SavePic();
+            savePic.SaveVisual(context.visualForTabItem1);
+        }
+        #endregion
+
     }
 }
