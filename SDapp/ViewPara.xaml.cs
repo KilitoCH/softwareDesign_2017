1aa05d8e6a22d3ebc2c13b65858ce9659dbed52f
Zixin Chen <zixinhustEIC@outlook.com>
2017-12-28 20:24:12 +0800

    完善了参数表格

--- SDapp/ViewPara.xaml.cs
+++ SDapp/ViewPara.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using Microsoft.Win32;
 
 namespace SoftwareDesign_2017
@@ -38,7 +39,7 @@ namespace SoftwareDesign_2017
                 else
                 {
                     BPSK_Sequence_Generate bpskSequenceGenerate = new BPSK_Sequence_Generate(Convert.ToDouble(context.FrequenceBpsk));//利用TextBox中输入的参数获取一个序列
-                    paramList.Add(new Param(name, bpskSequenceGenerate.GetPsdSequenceReal,null));
+                    paramList.Add(new Param(name, bpskSequenceGenerate.GetPsdSequenceReal, null));
                     context.nameList.Add(name);
                 }
             }
@@ -52,7 +53,7 @@ namespace SoftwareDesign_2017
                 else
                 {
                     BOC_Sequence_Generate bocSequenceGenerate = new BOC_Sequence_Generate(Convert.ToInt32(context.Alpha), Convert.ToInt32(context.Beta));//利用TextBox中输入的参数获取一个序列
-                    paramList.Add(new Param(name, bocSequenceGenerate.GetPsdSequenceReal,bocSequenceGenerate.GetAutocorrelationSequence));
+                    paramList.Add(new Param(name, bocSequenceGenerate.GetPsdSequenceReal, bocSequenceGenerate.GetAutocorrelationSequence));
                     context.nameList.Add(name);
                 }
             }
@@ -61,7 +62,7 @@ namespace SoftwareDesign_2017
             context.Beta = 0;
         }
 
-        private void Save_Button_Click(object sender,RoutedEventArgs e)
+        private void Save_Button_Click(object sender, RoutedEventArgs e)
         {
             string url;//将要保存文件的路径
 
@@ -73,7 +74,44 @@ namespace SoftwareDesign_2017
 
             var control = listView as Control;
             SavePic savePic = new SavePic();
-            savePic.SaveControl(control,url);
+            savePic.SaveControl(control, url);
+        }
+
+        private void Remove_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                context.nameList.RemoveAt(listView.SelectedIndex);
+                paramList.RemoveAt(listView.SelectedIndex);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("请先选择一项再点击删除按钮", "提示");
+            }
+                  
+        }
+
+        private void RemoveAll_Button_Click(object sender, RoutedEventArgs e)
+        {
+            context.nameList.Clear();
+            paramList.Clear();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var control = sender as TextBox;
+            if (control == null)
+                return;
+            control.SelectAll();
+        }
+
+        private void TextBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var control = sender as TextBox;
+            if (control == null)
+                return;
+            Keyboard.Focus(control);
+            e.Handled = true;
         }
     }
 }
